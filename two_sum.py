23b1a878413381596e3d8401c8b4dc6d7c70e0dc
Dan Sokolsky <dansokolsky@Dans-Air.home>
2017-06-18 17:50:56 -0400

    two_sum.py: complete solution

--- two_sum.py
+++ two_sum.py
@@ -3,7 +3,12 @@ def two_sum(l, target):
 	i = 0
 	j = len(l)-1
 	while i < j:
-		if l[i] + l[j] == target:
+		s = l[i] + l[j]
+		if s == target:
 			print l[i], l[j]
-		i += 1
-		j += 1
+			i += 1
+			j -= 1
+		elif s < target:
+			i += 1
+		else:
+			j -= 1
